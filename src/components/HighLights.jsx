bb8de685753ec7e3dfb996f9ee7727a86ada72cb
RazazanOfficial <RazazanOfficial@gmail.com>
2024-09-21 21:25:28 +0330

    2- HighLights & VideoCarousel Compelected

--- src/components/HighLights.jsx
+++ src/components/HighLights.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import React from 'react';
+import { rightImg, watchImg } from '../utils';
+import VideoCarousel from './VideoCarousel';
 
 const HighLights = () => {
-  return (
-    <div>HighLights</div>
-  )
-}
+  //=-=-=||gsap||=-=-=//
+  useGSAP(() => {
+    gsap.to('#title', { opacity: 1, y: 0 });
+    gsap.to('.link', { opacity: 1, y: 0, duration: 1, stagger: 0.25 });
+  }, []);
 
-export default HighLights
+  return (
+    <section
+      className="w-screen h-full overflow-hidden bg-zinc common-padding"
+      id="highlights"
+    >
+      <div className="screen-max-width">
+        <div className="mb-12 w-full md:flex items-end justify-between">
+          <h1 id="title" className="section-heading">
+            Get the Highlights
+          </h1>
+          <div className="flex flex-wrap items-end gap-5 mt-2">
+            <p className="link">
+              Watch the film
+              <img src={watchImg} alt="watch" className="ml-2" />
+            </p>
+            <p className="link">
+              Watch the event
+              <img src={rightImg} alt="right" className="ml-2" />
+            </p>
+          </div>
+        </div>
+        <VideoCarousel />
+      </div>
+    </section>
+  );
+};
+
+export default HighLights;
